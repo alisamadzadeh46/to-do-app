41bdcba56abfc6585cf0729f65794f733cd079f4
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-04-18 18:39:36 +0430

    create ToDoRepository.kt and update ToDoDao.kt

--- app/src/main/java/com/example/to_do_app/data/ToDoDao.kt
+++ app/src/main/java/com/example/to_do_app/data/ToDoDao.kt
@@ -24,10 +24,10 @@ interface ToDoDao {
     suspend fun deleteTask(toDoTask: ToDoTask)
 
     @Query("DELETE FROM todo_table")
-    suspend fun deleteAllTask(toDoTask: ToDoTask)
+    suspend fun deleteAllTask()
 
     @Query("SELECT *  FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery ")
-    suspend fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
 
     @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END ")
     fun sortByLowPriority(): Flow<List<ToDoTask>>

1e509df6b96b6a849b4f8481233d49f7a39dc83f
alisamadzadeh <alisamadzadeh46@gmail.com>
2022-04-21 10:25:01 +0430

    update

--- app/src/main/java/com/example/to_do_app/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/to_do_app/ui/screens/list/ListScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
@@ -17,6 +16,9 @@ fun ListScreen(
     navigateToTaskScreen: (Int) -> Unit
 ) {
     Scaffold(
+        topBar = {
+            DefaultListAppBar()
+        },
         content = {},
         floatingActionButton = {
             ListFab(onFabClicked = navigateToTaskScreen)
